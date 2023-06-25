be0b8c80ff3c1a835eb11cedf7f81256dd4f6040
nayeone <63294503+nayeone@users.noreply.github.com>
2023-06-25 14:03:21 +0900

    Update 백준_11650_nayeon.py

--- week7/백준_11650_nayeon.py
+++ week7/백준_11650_nayeon.py
@@ -1,1 +1,13 @@
+N = int(input())
+nlist = []
 
+for i in range(N):
+  x, y = input().split()
+  x = int(x)
+  y = int(y)
+  nlist.append([x, y])
+
+nlist.sort()
+
+for i in nlist:
+  print (i[0], i[1])
