fbb6b43fb6d9e67eeff0929204e3c1962014e1a1
nayeone <63294503+nayeone@users.noreply.github.com>
2023-07-04 14:48:43 +0900

    Update 백준_2512_nayeon.py

--- week8/백준_2512_nayeon.py
+++ week8/백준_2512_nayeon.py
@@ -1,1 +1,21 @@
+N = int(input())
 
+nlist = list(map(int, input().split()))
+maxN = int(input())
+
+start, end = 0, max(nlist)
+
+while start <= end:
+  mid = (start + end) // 2
+  sumN = 0
+  for i in nlist:
+    if i > mid:
+      sumN += mid
+    else:
+      sumN += i
+  if sumN > maxN:
+    end = mid - 1
+  else:
+    start = mid + 1
+
+print(end)
